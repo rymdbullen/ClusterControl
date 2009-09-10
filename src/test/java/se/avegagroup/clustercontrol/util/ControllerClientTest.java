3264a212038bb724dfb0625f68cbe8bad1aec312
Jan Stenvall <jan.stenvall@gmail.com>
2009-09-10 17:17:25 +0200

    yihaaa

---
+++ src/test/java/se/avegagroup/clustercontrol/util/ControllerClientTest.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package se.avegagroup.clustercontrol.util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import se.avegagroup.clustercontrol.data.JkBalancerType;
+import se.avegagroup.clustercontrol.data.JkMemberType;
+import se.avegagroup.clustercontrol.logic.ControllerClient;
+import junit.framework.TestCase;
+
+/**
+ * @author admin
+ *
+ */
+public class ControllerClientTest extends TestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	/**
+	 * Test method for {@link se.avegagroup.clustercontrol.util.WorkerStatus#unmarshall(java.lang.String)}.
+	 * @throws MalformedURLException 
+	 */
+	public void testInit() throws MalformedURLException {
+		String url = "http://localhost:8888/jkmanager";
+		URL urll = new URL(url);
+		
+		ArrayList<JkBalancerType> balancers = ControllerClient.init(urll);		
+		
+		for (int index = 0; index < balancers.size(); index++) {
+			JkBalancerType balancer = balancers.get(index);
+			assertNotNull(balancer);
+			List<JkMemberType> members = balancer.getMember();
+			Iterator<JkMemberType> membersIter = members.iterator();
+			while (membersIter.hasNext()) {
+				JkMemberType jkMember = (JkMemberType) membersIter.next();
+				System.out.println(jkMember.getName()+" "+jkMember.getActivation()+" "+jkMember.getState());
+			}
+		}
+	}
+	/**
+	 * Test method for {@link se.avegagroup.clustercontrol.util.WorkerStatus#unmarshall(java.lang.String)}.
+	 */
+	public void testActivateUnmarshall() {
+		String worker = "footprint1";
+		ArrayList<JkBalancerType> workerLists = ControllerClient.activate(worker);
+		for (int i = 0; i < workerLists.size(); i++) {
+			JkBalancerType workerList = workerLists.get(i);
+			for (int index = 0; index < workerList.getMemberCount(); index++) {
+				JkMemberType workerStatus = workerList.getMember().get(index);
+				System.out.println("["+index+"]: "+workerStatus.getActivation());
+				assertEquals("ACT", workerStatus.getActivation());
+			}
+		}
+	}
+	/**
+	 * Test method for {@link se.avegagroup.clustercontrol.util.WorkerStatus#unmarshall(java.lang.String)}.
+	 */
+	public void testDisableUnmarshall() {
+		String worker = "footprint1";
+		ArrayList<JkBalancerType> workerLists = ControllerClient.disable(worker);
+		for (int i = 0; i < workerLists.size(); i++) {
+			JkBalancerType workerList = workerLists.get(i);
+			for (int index = 0; index < workerList.getMemberCount(); index++) {
+				JkMemberType workerStatus = workerList.getMember().get(index);
+				System.out.println("["+index+"]: "+workerStatus.getActivation());
+				assertEquals("OK", workerStatus);
+			}
+		}
+	}
+}
