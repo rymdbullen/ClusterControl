ecf2adfa539d3cdc65ef98a4572c894761b75737
Jan Stenvall <jan.stenvall@gmail.com>
2009-09-18 22:28:15 +0200

    integration tests may be skipped by adding -Dmaven.integration-test.skip. refactored http exchange, added Response object

---
+++ src/test/java/se/avegagroup/clustercontrol/integration/ControllerClientTest.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+package se.avegagroup.clustercontrol.integration;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.client.HttpResponseException;
+
+import se.avegagroup.clustercontrol.domain.JkBalancerType;
+import se.avegagroup.clustercontrol.domain.JkMemberType;
+import se.avegagroup.clustercontrol.logic.WorkerManager;
+import junit.framework.TestCase;
+
+/**
+ * @author admin
+ *
+ */
+public class ControllerClientTest extends TestCase {
+
+	private static Log logger = LogFactory.getLog(ControllerClientTest.class);
+	//private static final Logger logger = LoggerFactory.getLogger(ControllerClientTest.class);
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	/**
+	 * Test method for {@link se.avegagroup.clustercontrol.logic.WorkerManager#init(java.net.URL)}.
+	 * @throws MalformedURLException 
+	 */
+	public void testInitDouble() throws MalformedURLException, HttpResponseException {
+		WorkerManager.reset();
+		logger.debug("Running testInit");
+		String url = "http://localhost:8888/jkmanager";
+		URL urll = new URL(url);
+		
+		ArrayList<JkBalancerType> balancers = WorkerManager.init(urll);
+		assertEquals(1,balancers.size());
+		
+		balancers = WorkerManager.init(urll);
+		assertEquals(1,balancers.size());
+	}		
+	/**
+	 * Test method for {@link se.avegagroup.clustercontrol.logic.WorkerManager#init(java.net.URL)}.
+	 * @throws MalformedURLException 
+	 */
+	public void testInit() throws MalformedURLException, HttpResponseException{
+		WorkerManager.reset();
+		logger.debug("Running testInit");
+		String url = "http://localhost:8888/jkmanager";
+		URL urll = new URL(url);
+		
+		ArrayList<JkBalancerType> balancers = WorkerManager.init(urll);		
+		
+		for (int index = 0; index < balancers.size(); index++) {
+			JkBalancerType balancer = balancers.get(index);
+			assertNotNull(balancer);
+			List<JkMemberType> members = balancer.getMember();
+			Iterator<JkMemberType> membersIter = members.iterator();
+			while (membersIter.hasNext()) {
+				JkMemberType jkMember = (JkMemberType) membersIter.next();
+				logger.debug(jkMember.getName()+" "+jkMember.getActivation()+" "+jkMember.getState());
+			}
+		}
+	}
+	/**
+	 * Test method for {@link se.avegagroup.clustercontrol.logic.WorkerManager#activate(String)}.
+	 */
+	public void testActivateUnmarshall() throws HttpResponseException {
+		logger.debug("Running testActivateUnmarshall");
+		String worker = "footprint1";
+		ArrayList<JkBalancerType> workerLists = WorkerManager.activate(worker);
+		for (int i = 0; i < workerLists.size(); i++) {
+			JkBalancerType workerList = workerLists.get(i);
+			for (int index = 0; index < workerList.getMemberCount(); index++) {
+				JkMemberType workerStatus = workerList.getMember().get(index);
+				if(worker.equals(workerStatus.getName())) {
+					assertEquals("DIS", workerStatus.getActivation());
+				}
+				logger.debug("["+index+"]: "+workerStatus.getName()+" "+workerStatus.getActivation());
+			}
+		}
+	}
+	/**
+	 * Test method for {@link se.avegagroup.clustercontrol.logic.WorkerManager#disable(String)}.
+	 */
+	public void testDisableUnmarshall() throws HttpResponseException {
+		logger.debug("Running testDisableUnmarshall");
+		String worker = "footprint1";
+		ArrayList<JkBalancerType> workerLists = WorkerManager.disable(worker);
+		for (int i = 0; i < workerLists.size(); i++) {
+			JkBalancerType workerList = workerLists.get(i);
+			for (int index = 0; index < workerList.getMemberCount(); index++) {
+				JkMemberType workerStatus = workerList.getMember().get(index);
+				if(worker.equals(workerStatus.getName())) {
+					assertEquals("DIS", workerStatus.getActivation());
+				}
+				logger.debug("["+index+"]: "+workerStatus.getName()+" "+workerStatus.getActivation());
+			}
+		}
+	}
+}
---
+++ src/test/java/se/avegagroup/clustercontrol/integration/WorkerStatusTest.java
@@ -0,0 +1,114 @@
+/**
+ * 
+ */
+package se.avegagroup.clustercontrol.integration;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.bind.JAXBElement;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import se.avegagroup.clustercontrol.domain.JkBalancerType;
+import se.avegagroup.clustercontrol.domain.JkBalancersType;
+import se.avegagroup.clustercontrol.domain.JkMemberType;
+import se.avegagroup.clustercontrol.domain.JkStatusType;
+import se.avegagroup.clustercontrol.domain.Hosts;
+import se.avegagroup.clustercontrol.domain.HostType;
+import se.avegagroup.clustercontrol.domain.WorkerResponse;
+import se.avegagroup.clustercontrol.domain.WorkerResponses;
+import se.avegagroup.clustercontrol.logic.WorkerManager;
+import se.avegagroup.clustercontrol.util.WorkerStatus;
+import junit.framework.TestCase;
+
+/**
+ * @author admin
+ *
+ */
+public class WorkerStatusTest extends TestCase {
+
+	private static Log logger = LogFactory.getLog(WorkerStatusTest.class);
+//	private static final Logger logger = LoggerFactory.getLogger(WorkerStatusTest.class);
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		String loadBalancer = "lbfootprint";
+		String hostname = "localhost";
+		
+		Hosts hosts = new Hosts();
+		HostType host = new HostType();
+		hosts.setLoadBalancer(loadBalancer);
+		host.setIpAddress(hostname);
+		host.setContext("jkmanager");
+		host.setPort(8888);
+		hosts.getHost().add(host);
+		
+		WorkerManager.init(hosts);
+	}
+	/**
+	 * Test method for {@link se.avegagroup.clustercontrol.util.WorkerStatus#unmarshall(java.lang.String)}.
+	 */
+	public void testGetStatusUnmarshall() {
+		
+		logger.debug("Running testGetStatusUnmarshall");
+		
+		WorkerResponses workerResponses = WorkerManager.status("xml");
+		
+		WorkerStatus workerStatus = new WorkerStatus();
+		int hostsCount = workerResponses.getWorkerStatus().size();
+		for (int hostIdx = 0; hostIdx < hostsCount; hostIdx++) {
+			WorkerResponse workerResponse = workerResponses.getWorkerStatus().get(hostIdx);
+			JAXBElement<JkStatusType> jkStatus = workerStatus.unmarshall(workerResponse.getBody());
+			assertNotNull(jkStatus);
+			assertEquals(new Integer(1), jkStatus.getValue().getBalancers().getCount());
+			JkBalancersType balancers =  jkStatus.getValue().getBalancers();
+			assertEquals(new Integer(2), balancers.getBalancer().getMemberCount());
+			List<JkMemberType> members = balancers.getBalancer().getMember();
+			Iterator<JkMemberType> membersIter = members.iterator();
+			while (membersIter.hasNext()) {
+				JkMemberType jkMember = (JkMemberType) membersIter.next();
+				logger.debug(jkMember.getName()+" "+jkMember.getActivation()+" "+jkMember.getState());
+			}
+		}
+	}
+	/**
+	 * Test method for {@link se.avegagroup.clustercontrol.util.WorkerStatus#unmarshall(java.lang.String)}.
+	 */
+	public void testActivateUnmarshall() {
+		logger.debug("Running testActivateUnmarshall");
+		String worker = "footprint1";
+		ArrayList<JkBalancerType> workerLists = WorkerManager.activate(worker);
+		for (int i = 0; i < workerLists.size(); i++) {
+			JkBalancerType workerList = workerLists.get(i);
+			for (int index = 0; index < workerList.getMemberCount(); index++) {
+				JkMemberType workerStatus = workerList.getMember().get(index);
+				if(worker.equals(workerStatus.getName())) {
+					assertEquals("ACT", workerStatus.getActivation());
+				}
+				logger.debug("["+index+"]: "+workerStatus.getName()+" "+workerStatus.getActivation());
+			}
+		}
+	}
+	/**
+	 * Test method for {@link se.avegagroup.clustercontrol.util.WorkerStatus#unmarshall(java.lang.String)}.
+	 */
+	public void testDisableUnmarshall() {
+		logger.debug("Running testDisableUnmarshall");
+		String worker = "footprint1";
+		ArrayList<JkBalancerType> workerLists = WorkerManager.disable(worker);
+		for (int i = 0; i < workerLists.size(); i++) {
+			JkBalancerType workerList = workerLists.get(i);
+			for (int index = 0; index < workerList.getMemberCount(); index++) {
+				JkMemberType workerStatus = workerList.getMember().get(index);
+				if(worker.equals(workerStatus.getName())) {
+					assertEquals("DIS", workerStatus.getActivation());
+				}
+				logger.debug("["+index+"]: "+workerStatus.getName()+" "+workerStatus.getActivation());
+			}
+		}
+	}
+}
