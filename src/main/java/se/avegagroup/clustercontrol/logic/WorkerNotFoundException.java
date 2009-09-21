9d7a103c269ac8a17e43c0864a844d97fcf9e76a
Jan Stenvall <jan.stenvall@gmail.com>
2009-09-21 23:56:28 +0200

    added exceptions when host/worker is not found

---
+++ src/main/java/se/avegagroup/clustercontrol/logic/WorkerNotFoundException.java
@@ -0,0 +1,10 @@
+package se.avegagroup.clustercontrol.logic;
+
+public class WorkerNotFoundException extends Exception {
+
+	/**
+	 * Generated Serial Verion UID
+	 */
+	private static final long serialVersionUID = -1432289180104926231L;
+	
+}
